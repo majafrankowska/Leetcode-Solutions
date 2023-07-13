5a7ebd22d3b688ee72100b45d438d02c6e1f00b7
Maja Frankowska <majafrankowska88@gmail.com>
2023-07-14 01:36:49 +0200

    Create Richest_Customer_Wealth.java
    1672. Richest Customer Wealth
    Difficulty - easy

---
+++ Richest_Customer_Wealth.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int[] arr = new int [accounts.length];
+        int num = 0;
+        for (int i = 0; i<accounts.length; i++){
+          for (int j = 0; j<account.length; j++){
+
+            num += accounts[i][j];
+
+
+
+          }
+          arr[i]= num;
+        }
+
+        for (int i = 0; i<arr.length; i++){
+
+          
+        }
+
+
+    }
+}
