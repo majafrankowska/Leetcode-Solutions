628bb6f15f589c0a9ffcf8419f03e9c89210769d
Maja Frankowska <majafrankowska88@gmail.com>
2023-07-29 01:07:56 +0200

    Update Predict_the_Winner.cpp
    486. Predict the Winner
    Difficulty - Medium

--- Predict_the_Winner.cpp
+++ Predict_the_Winner.cpp
@@ -1,3 +1,21 @@
-
-486. Predict the Winner
-Difficulty - Medium
+class Solution {
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+        
+        const int n = nums.size();
+        vector<vector<int>> v(n, vector<int>(n));
+        
+        for (int i = 0; i < n; ++i) {
+            v[i][i] = nums[i];
+        }
+        
+        for ( int i = 1; i < n; ++i) {
+            for (int j = 0; i + j < n; ++j) {
+                const int k = i + j;
+                v[j][k] = max(nums[j] - v[j + 1][k], nums[k] - v[j][k - 1]);
+            }
+        }
+        
+        return v[0][n - 1] >= 0;
+    }
+};
