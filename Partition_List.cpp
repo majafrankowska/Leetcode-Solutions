521ee3ff4864c2975c469354ced9ac3e14849fc7
Maja Frankowska <majafrankowska88@gmail.com>
2023-08-15 07:44:55 +0200

    Create Partition_List.cpp
    86. Partition List
    Difficulty - Medium

---
+++ Partition_List.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+         ListNode beforeHead(0);
+    ListNode afterHead(0);
+    ListNode* before = &beforeHead;
+    ListNode* after = &afterHead;
+
+    for (; head; head = head->next)
+      if (head->val < x) {
+        before->next = head;
+        before = head;
+      } else {
+        after->next = head;
+        after = head;
+      }
+
+    after->next = nullptr;
+    before->next = afterHead.next;
+
+    return beforeHead.next;
+    }
+};
