187a044199e838ddbdf73efbcff6c8c77cfa7f79
Maja Frankowska <majafrankowska88@gmail.com>
2023-09-03 12:58:08 +0200

    Create Unique_Paths.java
    62. Unique Paths
    Difficulty - Medium

---
+++ Unique_Paths.java
@@ -0,0 +1,21 @@
+/*
+62. Unique Paths
+Difficulty - Medium 
+
+Unique_Paths.java
+*/
+
+class Solution {
+  public int uniquePaths(int m, int n) {
+
+    int[][] dp = new int[m][n];
+    Arrays.stream(dp).forEach(A -> Arrays.fill(A, 1));
+
+    for (int i = 1; i < m; ++i)
+      for (int j = 1; j < n; ++j)
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+
+    return dp[m - 1][n - 1];
+    
+  }
+}
