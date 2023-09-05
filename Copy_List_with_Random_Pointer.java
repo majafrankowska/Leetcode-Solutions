3c31d91026be8588a97e3423677d56458fe57e30
Maja Frankowska <majafrankowska88@gmail.com>
2023-09-05 20:27:12 +0200

    Update Copy_List_with_Random_Pointer.java
    138. Copy List with Random Pointer
    Difficulty - Medium

--- Copy_List_with_Random_Pointer.java
+++ Copy_List_with_Random_Pointer.java
@@ -6,6 +6,26 @@ Difficulty - Medium
 Copy_List_with_Random_Pointer.java
 */
 
+class Solution {
+  public Node copyRandomList(Node head) {
+    
+    if (head == null)
+      return null;
+    if (map.containsKey(head))
+      return map.get(head);
+
+    Node newNode = new Node(head.val);
+    map.put(head, newNode);
+    newNode.next = copyRandomList(head.next);
+    newNode.random = copyRandomList(head.random);
+    
+    return newNode;
+    
+  }
+
+  private Map<Node, Node> map = new HashMap<>();
+  
+}
 
 
 
