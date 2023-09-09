3bb4dc34649ac9122aad9ebbee661909c036060d
Maja Frankowska <majafrankowska88@gmail.com>
2023-09-09 05:46:35 +0200

    Update Search_Insert_Position.java
    35. Search Insert Position
    Difficulty: Easy

--- Search_Insert_Position.java
+++ Search_Insert_Position.java
@@ -1,1 +1,28 @@
+/*
+35. Search Insert Position
+Difficulty: Easy 
+
+Search_Insert_Position.java
+*/
+
+class Solution {
+  public int searchInsert(int[] nums, int target) {
+      
+    int l = 0;
+    int r = nums.length;
+
+    while (l < r) {
+      final int m = (l + r) / 2;
+      if (nums[m] == target)
+        return m;
+      if (nums[m] < target)
+        l = m + 1;
+      else
+        r = m;
+    }
+
+    return l;
+      
+  }
+}
 
