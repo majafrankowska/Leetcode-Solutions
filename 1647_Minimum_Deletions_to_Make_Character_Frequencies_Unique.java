bf0db3618500c3883783a2925e1283dbf59f09e3
Maja Frankowska <majafrankowska88@gmail.com>
2023-09-13 02:28:25 +0200

    Create 1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique.java

---
+++ 1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique.java
@@ -0,0 +1,14 @@
+/*
+1647. Minimum Deletions to Make Character Frequencies Unique
+Difficulty: Medium
+
+1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique.java
+*/
+
+
+class Solution {
+public:
+    int minDeletions(string s) {
+        
+    }
+};
