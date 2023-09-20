660ca5dde687489a63e70a191a7e367da46fc844
Maja Frankowska <majafrankowska88@gmail.com>
2023-09-20 16:51:04 +0200

    Create 1658_Minimum_Operations_to_Reduce_X_to_Zero.py

---
+++ 1658_Minimum_Operations_to_Reduce_X_to_Zero.py
@@ -0,0 +1,28 @@
+# 1658. Minimum Operations to Reduce X to Zero
+# Difficulty: Medium
+
+# 1658_Minimum_Operations_to_Reduce_X_to_Zero.py
+
+class Solution:
+  def minOperations(self, nums: List[int], x: int) -> int:
+    targetSum = sum(nums) - x
+    if targetSum == 0:
+      return len(nums)
+    maxLen = self._maxSubArrayLen(nums, targetSum)
+    return -1 if maxLen == -1 else len(nums) - maxLen
+
+  # Same as 325. Maximum Size Subarray Sum Equals k
+  def _maxSubArrayLen(self, nums: List[int], k: int) -> int:
+    res = -1
+    prefix = 0
+    prefixToIndex = {0: -1}
+
+    for i, num in enumerate(nums):
+      prefix += num
+      target = prefix - k
+      if target in prefixToIndex:
+        res = max(res, i - prefixToIndex[target])
+      # No need to check the existence of the prefix since it's unique.
+      prefixToIndex[prefix] = i
+
+    return res
