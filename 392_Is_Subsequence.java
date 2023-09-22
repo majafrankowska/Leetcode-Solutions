10db9a360c2477b812fc840740847cd13cbb20c1
Maja Frankowska <majafrankowska88@gmail.com>
2023-09-22 23:01:32 +0200

    Update and rename 000nowe to 392_Is_Subsequence.java

---
+++ 392_Is_Subsequence.java
@@ -0,0 +1,25 @@
+/*
+392. Is Subsequence
+Difficulty: Easy 
+
+392_Is_Subsequence.java
+*/
+
+class Solution {
+  public boolean isSubsequence(String s, String t) {
+    
+    if (s.isEmpty())
+      return true;
+
+    int i = 0;
+    
+    for (final char c : t.toCharArray()) {
+      if (s.charAt(i) == c && ++i == s.length()) {
+        return true;
+      }
+    }
+
+    return false;
+    
+  }
+}
