ee5ca925c7423276a7b5010498156759625c8042
Maja Frankowska <majafrankowska88@gmail.com>
2023-09-28 21:55:53 +0200

    Create 905_Sort_Array_By_Parity.java

---
+++ 905_Sort_Array_By_Parity.java
@@ -0,0 +1,29 @@
+/*
+905. Sort Array By Parity
+Difficulty: Easy
+
+905_Sort_Array_By_Parity.java
+*/
+
+class Solution {
+  public int[] sortArrayByParity(int[] nums) {
+    
+    int l = 0;
+    int r = nums.length - 1;
+
+    while (l < r) {
+      if (nums[l] % 2 == 1 && nums[r] % 2 == 0) {
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
+      }
+      if (nums[l] % 2 == 0)
+        ++l;
+      if (nums[r] % 2 == 1)
+        --r;
+    }
+
+    return nums;
+    
+  }
+}
