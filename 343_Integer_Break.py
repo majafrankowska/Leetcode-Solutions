a29303bbd28ba73576aa03dec661769f58390925
Maja Frankowska <majafrankowska88@gmail.com>
2023-10-06 03:05:10 +0200

    Create 343_Integer_Break.py

---
+++ 343_Integer_Break.py
@@ -0,0 +1,21 @@
+//343. Integer Break
+//Difficulty: Medium
+
+//343_Integer_Break.py
+
+class Solution:
+  def integerBreak(self, n: int) -> int:
+    
+    if n == 2:
+      return 1
+    if n == 3:
+      return 2
+
+    ans = 1
+
+    while n > 4:
+      n -= 3
+      ans *= 3
+    ans *= n
+
+    return ans
