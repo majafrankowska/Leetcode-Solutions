658a739fa168abfee4f706d272b87046a5b3af7e
Maja Frankowska <majafrankowska88@gmail.com>
2023-10-13 04:15:00 +0200

    Create 746_Min_Cost_Climbing_Stairs.java

---
+++ 746_Min_Cost_Climbing_Stairs.java
@@ -0,0 +1,19 @@
+/*
+746. Min Cost Climbing Stairs
+Difficulty: Easy
+
+746_Min_Cost_Climbing_Stairs.java
+*/
+
+class Solution {
+  public int minCostClimbingStairs(int[] cost) {
+    
+    final int n = cost.length;
+
+    for (int i = 2; i < n; ++i)
+      cost[i] += Math.min(cost[i - 1], cost[i - 2]);
+
+    return Math.min(cost[n - 1], cost[n - 2]);
+    
+  }
+}
