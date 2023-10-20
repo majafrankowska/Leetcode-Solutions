83b8f926131f223e4e58a5bae6c640bad58a6680
Maja Frankowska <majafrankowska88@gmail.com>
2023-10-21 00:12:28 +0200

    Update 341_Flatten_Nested_List_Iterator.java

--- 341_Flatten_Nested_List_Iterator.java
+++ 341_Flatten_Nested_List_Iterator.java
@@ -5,3 +5,28 @@ Difficulty:
 341_Flatten_Nested_List_Iterator.java
 */
 
+public class NestedIterator implements Iterator<Integer> {
+  public NestedIterator(List<NestedInteger> nestedList) {
+    addInteger(nestedList);
+  }
+
+  @Override
+  public Integer next() {
+    return q.poll();
+  }
+
+  @Override
+  public boolean hasNext() {
+    return !q.isEmpty();
+  }
+
+  private Queue<Integer> q = new ArrayDeque<>();
+
+  private void addInteger(final List<NestedInteger> nestedList) {
+    for (final NestedInteger ni : nestedList)
+      if (ni.isInteger())
+        q.offer(ni.getInteger());
+      else
+        addInteger(ni.getList());
+  }
+}
