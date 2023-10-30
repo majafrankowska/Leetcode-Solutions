8f51a71d59b5493027ccfbd4258d89640d9e7da6
Maja Frankowska <majafrankowska88@gmail.com>
2023-10-31 00:33:48 +0100

    Update and rename 458_Poor_Pigs.java to 458_Poor_Pigs.cpp

---
+++ 458_Poor_Pigs.cpp
@@ -0,0 +1,16 @@
+/*
+458. Poor Pigs
+Difficulty: Hard
+
+458_Poor_Pigs.cpp
+*/
+
+
+class Solution {
+public:
+  int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+    int times = minutesToTest / minutesToDie;
+    int result = 0;
+    while (pow(times + 1, result) < buckets) ++result;    return result;
+  }
+};
