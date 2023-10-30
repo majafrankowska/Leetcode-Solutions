75f7a47ea1d89d4ca0a1c8e7e0e5e8c39792f431
Maja Frankowska <majafrankowska88@gmail.com>
2023-10-31 00:36:14 +0100

    Create 1356_Sort_Integers_by_The_Number_of_1_Bits.java

---
+++ 1356_Sort_Integers_by_The_Number_of_1_Bits.java
@@ -0,0 +1,18 @@
+/*
+1356. Sort Integers by The Number of 1 Bits
+Difficulty:
+
+1356_Sort_Integers_by_The_Number_of_1_Bits.java
+*/
+
+class Solution {
+ public:
+  vector<int> sortByBits(vector<int>& arr) {
+    ranges::sort(arr, [](const int a, int b) {
+      const int x = bitset<32>(a).count();
+      const int y = bitset<32>(b).count();
+      return x == y ? a < b : x < y;
+    });
+    return arr;
+  }
+};
