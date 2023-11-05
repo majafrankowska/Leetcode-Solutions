128ca4149dd89d75b68bbfed28692a544769c667
Maja Frankowska <majafrankowska88@gmail.com>
2023-11-06 00:42:53 +0100

    Update 1535_Find_the_Winner_of_an_Array_Game.java

--- 1535_Find_the_Winner_of_an_Array_Game.java
+++ 1535_Find_the_Winner_of_an_Array_Game.java
@@ -1,7 +1,29 @@
 /*
 1535. Find the Winner of an Array Game
-Difficulty: 
+Difficulty: Medium
 
 1535_Find_the_Winner_of_an_Array_Game.java
 */
 
+class Solution {
+    public int getWinner(int[] arr, int k) {
+        
+        int ans = arr[0];
+        int wins = 0;
+        
+        for (int i = 1; i < arr.length && wins < k; ++i){
+            if (arr[i] > ans) {
+                ans = arr[i];
+                wins = 1;
+            }
+            
+            else {
+                ++wins;
+                
+            }
+        }
+        
+        return ans;
+        
+    }
+}
