d938cb6562a014aef3b2b0328c04dbe62d0c3225
Maja Frankowska <majafrankowska88@gmail.com>
2023-11-13 02:54:23 +0100

    Update 2785_Sort_Vowels_in_a_String.java

--- 2785_Sort_Vowels_in_a_String.java
+++ 2785_Sort_Vowels_in_a_String.java
@@ -4,3 +4,27 @@
 Difficulty: Medium
 */
 
+class Solution {
+  public String sortVowels(String s) {
+      
+    StringBuilder sb = new StringBuilder();
+    List<Character> vowels = new ArrayList<>();
+
+    for (final char c : s.toCharArray())
+      if (isVowel(c))
+        vowels.add(c);
+
+    Collections.sort(vowels);
+
+    int i = 0; 
+    for (final char c : s.toCharArray())
+      sb.append(isVowel(c) ? vowels.get(i++) : c);
+
+    return sb.toString();
+  }
+
+  private boolean isVowel(char c) {
+    return "aeiouAEIOU".indexOf(c) != -1;
+      
+  }
+}
