4b09f2c44a5c88fc739a74b7fbdfa53b35f4655d
Maja Frankowska <majafrankowska88@gmail.com>
2023-11-13 02:40:23 +0100

    Update 815_Bus_Routes.java

--- 815_Bus_Routes.java
+++ 815_Bus_Routes.java
@@ -1,8 +1,41 @@
 /*
 815_Bus_Routes.java
-
-
 815. Bus Routes
+Difficulty: Hard
 */
 
-//multiple clients, żeby serwer przyjmował wielu klientów
+class Solution {
+  public int numBusesToDestination(int[][] routes, int source, int target) {
+      
+    if (source == target)
+      return 0;
+
+    Map<Integer, List<Integer>> graph = new HashMap<>();
+    Set<Integer> usedBuses = new HashSet<>();
+
+    for (int i = 0; i < routes.length; ++i)
+      for (final int route : routes[i]) {
+        graph.putIfAbsent(route, new ArrayList<>());
+        graph.get(route).add(i);
+      }
+
+    int ans = 0;
+    Queue<Integer> q = new ArrayDeque<>(Arrays.asList(source));
+
+    while (!q.isEmpty()) {
+      ++ans;
+      for (int sz = q.size(); sz > 0; --sz) {
+        for (final int bus : graph.get(q.poll()))
+          if (usedBuses.add(bus))
+            for (final int nextRoute : routes[bus]) {
+              if (nextRoute == target)
+                return ans;
+              q.offer(nextRoute);
+            }
+      }
+    }
+
+    return -1;
+      
+  }
+}
