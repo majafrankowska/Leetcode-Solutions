6d352a84bd5bbb005bbe651ef667a7371898fffd
Maja Frankowska <majafrankowska88@gmail.com>
2023-11-17 00:44:49 +0100

    Create 1980_Find_Unique_Binary_String.java

---
+++ 1980_Find_Unique_Binary_String.java
@@ -0,0 +1,24 @@
+/*
+1980_Find_Unique_Binary_String.java
+1980. Find Unique Binary String
+Difficulty: Medium
+*/
+
+class Solution {
+  public String findDifferentBinaryString(String[] nums) {
+
+    final int bitSize = nums[0].length();
+    final int maxNum = 1 << bitSize;
+    Set<Integer> numsSet = Arrays.stream(nums)
+                               .mapToInt(num -> Integer.parseInt(num, 2))
+                               .boxed()
+                               .collect(Collectors.toSet());
+
+    for (int num = 0; num < maxNum; ++num)
+      if (!numsSet.contains(num))
+        return String.format("%" + bitSize + "s", Integer.toBinaryString(num)).replace(' ', '0');
+
+    throw new IllegalArgumentException();
+
+  }
+}
