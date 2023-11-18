85a508e0f7095dda9ae69bbcb3832103555d8414
Maja Frankowska <majafrankowska88@gmail.com>
2023-11-19 00:11:36 +0100

    Create 1838_Frequency_of_the_Most_Frequent_Element.java

---
+++ 1838_Frequency_of_the_Most_Frequent_Element.java
@@ -0,0 +1,26 @@
+/*
+1838_Frequency_of_the_Most_Frequent_Element.java
+1838. Frequency of the Most Frequent Element
+Difficulty: Medium
+*/
+
+class Solution {
+  public int maxFrequency(int[] nums, int k) {
+      
+    int ans = 0;
+    long sum = 0;
+
+    Arrays.sort(nums);
+
+    for (int l = 0, r = 0; r < nums.length; ++r) {
+      sum += nums[r];
+      while (sum + k < (long) nums[r] * (r - l + 1))
+        sum -= nums[l++];
+      ans = Math.max(ans, r - l + 1);
+    }
+
+    return ans;
+      
+  }
+}
+
