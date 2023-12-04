815f752f2516438fd4355cf52f68d45bb25c9577
Maja Frankowska <majafrankowska88@gmail.com>
2023-12-04 22:55:17 +0100

    Create 1266_Minimum_Time_Visiting_All_Points.java

---
+++ 1266_Minimum_Time_Visiting_All_Points.java
@@ -0,0 +1,17 @@
+/*
+1266_Minimum_Time_Visiting_All_Points.java
+1266. Minimum Time Visiting All Points
+Difficulty: Easy
+*/
+
+class Solution {
+  public int minTimeToVisitAllPoints(int[][] points) {
+    int ans = 0;
+
+    for (int i = 1; i < points.length; ++i)
+      ans += Math.max(Math.abs(points[i][0] - points[i - 1][0]),
+                      Math.abs(points[i][1] - points[i - 1][1]));
+
+    return ans;
+  }
+}
