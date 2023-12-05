bd107d9b1fea41d0eea7a16f2180e78427fb09c6
Maja Frankowska <majafrankowska88@gmail.com>
2023-12-05 23:40:33 +0100

    Create 1688_Count_of_Matches_in_Tournament.java

---
+++ 1688_Count_of_Matches_in_Tournament.java
@@ -0,0 +1,13 @@
+/*
+1688_Count_of_Matches_in_Tournament.java
+1688. Count of Matches in Tournament
+Difficulty: Easy
+*/
+
+class Solution {
+  public int numberOfMatches(int n) {
+      
+    return n - 1;
+      
+  }
+}
