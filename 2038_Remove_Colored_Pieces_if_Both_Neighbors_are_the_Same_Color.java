e3240b00a5c3b127e5c9f6866b1d1f5110bd31e6
Maja Frankowska <majafrankowska88@gmail.com>
2024-02-11 21:48:25 +0100

    Update 2038_Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.java

--- 2038_Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.java
+++ 2038_Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.java
@@ -1,5 +1,5 @@
 /*
-2038. Remove Colored Pieces if Both Neighbors are the Same Color
+2038. Remove Colored Pieces if Both Neighbors are the Same Color 
 Difficulty: Medium
 
 2038_Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.java
