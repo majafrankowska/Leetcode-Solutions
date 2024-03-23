cfb1baaf6cb52afb00576b4312d1b1307a32690e
Maja Frankowska <majafrankowska88@gmail.com>
2024-03-23 23:54:03 +0100

    Update 1220_Count_Vowels_Permutation.py

--- 1220_Count_Vowels_Permutation.py
+++ 1220_Count_Vowels_Permutation.py
@@ -7,7 +7,7 @@ class Solution:
   def countVowelPermutation(self, n: int) -> int:  
     kMod = 1_000_000_007
     dp = {'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1}
-
+ 
     for _ in range(n - 1):
       newDp = {'a': dp['e'] + dp['i'] + dp['u'], 
                'e': dp['a'] + dp['i'],
