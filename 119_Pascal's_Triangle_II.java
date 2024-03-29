a3b0298033fdf3ef317b9a13c310cf4ce951ca56
Maja Frankowska <majafrankowska88@gmail.com>
2024-03-29 22:51:50 +0100

    Update 119_Pascal's_Triangle_II.java

--- 119_Pascal's_Triangle_II.java
+++ 119_Pascal's_Triangle_II.java
@@ -10,7 +10,7 @@ class Solution {
        
     Integer[] ans = new Integer[rowIndex + 1]; 
     Arrays.fill(ans, 1);
-
+ 
     for (int i = 2; i < rowIndex + 1; ++i)
       for (int j = 1; j < i; ++j)
         ans[i - j] += ans[i - j - 1];
