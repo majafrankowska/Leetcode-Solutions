66d4b8e88faa7ce7751f61d0dfaeb8bef12ce0c9
Maja Frankowska <majafrankowska88@gmail.com>
2024-04-19 19:36:29 +0200

    Update 1095_Find_in_Mountain_Array.java

--- 1095_Find_in_Mountain_Array.java
+++ 1095_Find_in_Mountain_Array.java
@@ -7,7 +7,7 @@ Difficulty: Hard
  
 /**
  * // This is MountainArray's API interface.
- * // You should not implement it, or speculate about its implementation
+ * // You should not implement it, or speculate about its implementation 
  * interface MountainArray {
  *   public int get(int index) {}
  *   public int length() {} 
