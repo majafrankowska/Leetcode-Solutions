e5fe8e52f7d7b4037e8b0b06ebd8aa78ac1046a9
Maja Frankowska <majafrankowska88@gmail.com>
2024-06-10 23:42:13 +0200

    Update 1048_Longest_String_Chain.java

--- 1048_Longest_String_Chain.java
+++ 1048_Longest_String_Chain.java
@@ -5,7 +5,7 @@ Difficulty: Medium
 */
 
 class Solution {     
-  public int longestStrChain(String[] words) {     
+  public int longestStrChain(String[] words) {      
          
     int ans = 0; 
     Map<String, Integer> dp = new HashMap<>();   
