a8e012874ddc0369afcdabb7bee040abaa64d35d
Maja Frankowska <majafrankowska88@gmail.com>
2025-01-30 00:41:43 +0100

    Update 195_TenthLine.sh

--- 195_TenthLine.sh
+++ 195_TenthLine.sh
@@ -1,10 +1,10 @@
 # Read from the file file.txt and output the tenth line to stdout.
 
-count = 0
+count=0
 
 while read line; do
     let 'count = count + 1'
-    if [$count -eq 10]; then 
+    if [ $count -eq 10 ]; then 
         echo $line 
         exit 0
     fi
